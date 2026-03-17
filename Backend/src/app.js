49caeea091a26d5bbd0776212cbee7f9b45baafd
Raj Kamal <rajkamaldhan@gmail.com>
2026-03-17 17:45:14 +0530

    feat: integrate Socket.io for real-time communication and add socket connection handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,14 +9,16 @@ const app = express()
 // Middleware
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
+app.use(express.json())
 app.use(morgan("dev"))
+
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", 'POST', 'PUT', 'DELETE'],
-}))
-
-app.use(express.json())
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 // Health check 
